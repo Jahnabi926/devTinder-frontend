89f4aeadb48a886d29d1ca963e43d0dc37706291
Jahnabi Sarma <sarmajahnabi8@gmail.com>
2026-07-10 10:18:16 +0530

    feat: login page with axios authentication

--- README.md
+++ README.md
@@ -10,3 +10,9 @@
 -- Create routes and it children routes
 -- Create an Outlet in your Body component
 -- Create a Footer
+-- Create a Login Page
+-- Install axios
+-- CORS - Install cors in backend => add middleware to app with configurations: origin , credentials: true
+-- Whenever you're making an API call using axios, pass {withCredentials : true}. It will not send the token back to the Application tab of browser. Cookies won't have the token. Set-cookies will be seen in Networks tab Header but not in Applications tab.
+-- Send the user data in backend, instaed of "Login Successful" message.
+-- Test your code. Login successful saves token under Applications tab.
--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,5 +1,70 @@
+import { useState } from "react";
+import axios from "axios";
+
 const Login = () => {
-  return <div>Login</div>;
+  const [emailId, setEmailId] = useState("jahnabi@gmail.com");
+  const [password, setPassword] = useState("Jahnabi@1010");
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        "http://localhost:7777/login",
+        {
+          emailId,
+          password,
+        },
+        { withCredentials: true },
+      );
+      console.log("User logged in:", res.data);
+    } catch (error) {
+      console.log("Login failed - ERROR : ", error.message);
+    }
+  };
+  return (
+    <div className="flex justify-center my-10">
+      <div className="card bg-base-300 w-96 shadow-sm hover:shadow-xl hover:-translate-y-1 transition-all duration-300">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login</h2>
+          <fieldset className="fieldset">
+            <label className="label" htmlFor="email">
+              EmailId
+            </label>
+            <input
+              type="text"
+              id="email"
+              className="input"
+              value={emailId}
+              onChange={(e) => {
+                console.log("Email typed:", e.target.value);
+                setEmailId(e.target.value);
+              }}
+            />
+          </fieldset>
+          <fieldset className="fieldset">
+            <label className="label" htmlFor="password">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="input"
+              value={password}
+              onChange={(e) => {
+                console.log("Password typed:", e.target.value);
+                setPassword(e.target.value);
+              }}
+            />
+          </fieldset>
+
+          <div className="card-actions justify-center my-5">
+            <button className="btn btn-primary" onClick={handleLogin}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
